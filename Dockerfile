d0408400125ed374bd4acf8aaff4aa14e82ccc37
Sébastien LECACHEUR <slecache@gmail.com>
2020-12-02 23:22:02 +0100

    level 6

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM alpine
-COPY entrypoint.sh /
-CMD ["/entrypoint.sh"]
+COPY secret.sh /
+ENTRYPOINT ["echo","-e","docker-game\n===========\n\nI'm still locked.\n\nLevel 6\n\nYou should override the default ENTRYPOINT to look at this /secret.sh and unlock this level.\n"]
---
+++ secret.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cat << EOM
+docker-game
+===========
+
+Well done. You proven that you know how to override default ENTRYPOINT of an image.
+Containers are used to run servers. And servers are useful when they can be reached to process requests.
+
+Level 7
+
+Do you know that you can inspect an image? Take a look to the description label of this server: docker run slecache/docker-game:server
+EOM
