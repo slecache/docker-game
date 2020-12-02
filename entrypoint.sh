03b8794d91668acac46b85da393e94d9ee47c502
Sébastien LECACHEUR <slecache@gmail.com>
2020-12-03 00:09:10 +0100

    level 10

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
+
+for j in `seq 1 1000`; do echo "docker docker docker docker docker docker docker docker docker docker";done
+
 cat << EOM
 docker-game
 ===========
 
-Level 1
+Congratulations! You completed our Docker Game!
 
-Your first task is to run the container whose docker image tag is the answer to this question:
+Finish line
 
-> Which animal is represented on the Docker logo?
+Well you:
+
+- started a container with "docker run"
+- started a container from label
+- used the interactive process with "-ti" option
+- entered in container with "docker exec"
+- mounted folder inside a container on host machine with "-v" option
+- overrided default CMD
+- overrided default ENTRYPOINT
+- inspected an image
+- exposed container port on host machine with "-p" option
+- scanned container's log
+
+Docker is extremely important tool to learn and use, 
+Especially when deploying complex systems.
+It is our hope that you continue to practice your docker skills so that you can one day become the ultimate docker master!
+
+Thanks for playing!
+
+PS: Maybe an advanced mode or easter eggs will arrive. ;-)
 
 EOM
+
+for i in `seq 1 972`; do echo "docker docker docker docker docker docker docker docker docker docker"; done
