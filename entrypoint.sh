86932b896a16471050a2bbc4c4b32377b884f444
Sébastien LECACHEUR <slecache@gmail.com>
2020-12-04 00:15:47 +0100

    insert new level 9

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-cat << EOM
+#!/bin/sh
+
+if [ -f /restart ]
+then
+  cat << EOM
 docker-game
 ===========
 
-Level 1
+Level 10
 
-Your first task is to run the container whose docker image tag is the answer to this question:
-
-> Which animal is represented on the Docker logo?
+It's time to take a look to the logs. But will you be able to take all theses lines? Tips: check in the last 1000 lines for this container: docker run slecache/docker-game:flood
 
 EOM
+else
+  cat << EOM
+docker-game
+===========
+
+Level 9
+
+This container is stopped. Start it again to progress. Naming a container can help to start it many times (and don't forget to attach me).
+
+EOM
+  touch /restart
+fi
